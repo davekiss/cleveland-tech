27525499112cc93af6148cdabd8b82a62fa978cc
Dave Kiss <dkiss@mux.com>
2022-12-08 22:17:51 -0500

    Revert this stuffs

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,5 @@ import sveltePreprocess from 'svelte-preprocess'
 export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: sveltePreprocess({
-    replace: [["process.env.FORM_ENDPONT", process.env.FORM_ENDPONT], ["process.env.RECAPTCHA_SITE_KEY", process.env.RECAPTCHA_SITE_KEY]],
-  }),
+  preprocess: sveltePreprocess()
 }
